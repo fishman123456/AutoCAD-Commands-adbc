e4bd6c0f60a5181daa72a7933b92dee3400b0e5a
Denis Borisovich Fishman <106389581+fishman123456@users.noreply.github.com>
2023-12-12 13:05:15 +0700

    добавил библиотеку для вставки блока, код не реализован

---
+++ BlockInsert/BlockInsert.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// This line is not mandatory, but improves loading performances
+[assembly: CommandClass(typeof(ACADCommands.BlockInsert))]
+
+namespace ACADCommands
+{
+    // This line is not mandatory, but improves loading performances
+    public class BlockInsert
+    {
+        // реализовать код для вставки блока
+    }
+}
