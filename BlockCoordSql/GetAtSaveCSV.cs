46e415f0291ee1ae6f361dd26ea88f8b2b26b808
fishman123456 <den9152@yandex.ru>
2023-12-17 18:14:29 +0700

    [CommandMethod("ListCSV")]
    Используем метод для создания строки

--- BlockCoordSql/GetAtSaveCSV.cs
+++ BlockCoordSql/GetAtSaveCSV.cs
@@ -1,19 +1,23 @@
-﻿using Autodesk.AutoCAD.ApplicationServices;
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Runtime;
-
+using System.Collections.Generic;
+using System.Linq;
+using System;
 using System.Text;
+using System.Threading.Tasks;
 
-// Эта строка не является обязательной, но улучшает производительность загрузки
-[assembly: CommandClass(typeof(ACADCommands.GetAttribValue))]
+
+[assembly: CommandClass(typeof(ACADCommands.GetAtSaveCSV))]
+
 
 namespace ACADCommands
 {
-    public class GetAttribValue
+    public class GetAtSaveCSV
     {
-        [CommandMethod("LISTATT")]
-        public static void ListAttributes()
+        [CommandMethod("ListCSV")]
+        public static void ListAttrSaveCSV()
         {
             CheckDateWork.CheckDate();
             Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
@@ -35,37 +39,36 @@ namespace ACADCommands
                 if (res.Status != PromptStatus.OK)
                     return;
                 SelectionSet selSet = res.Value;
+                // добавляем в массив выбранные обьекты
                 ObjectId[] idArray = selSet.GetObjectIds();
+                // строка для сохранения в csv
                 StringBuilder stringBuilder = new StringBuilder();
-
+                // перебираем блоки
                 foreach (ObjectId blkId in idArray)
                 {
                     BlockReference blkRef = (BlockReference)tr.GetObject(blkId, OpenMode.ForRead);
 
                     BlockTableRecord btr = (BlockTableRecord)tr.GetObject(blkRef.BlockTableRecord, OpenMode.ForRead);
-
-                   // btr.Dispose();
                     AttributeCollection attCol = blkRef.AttributeCollection;
-                    //  выводим координаты блока,слой и handle
-
-                    string str = ("\n--------------------------\n" +
-                                       "Block name: " + btr.Name + ",\n" +
-                                       "ID: " + blkRef.Id.ToString() + ",\n" +
-                                       "X: " + blkRef.Position.X.ToString() + ",\n" +
-                                       "Y: " + blkRef.Position.Y.ToString() + ",\n" +
-                                       "Z: " + blkRef.Position.Z.ToString() + ",\n" +
-                                       "Handle BlockRef : " + blkRef.Handle.ToString() + ",\n" + // вот нужная фигня - Handle
-                                       "Layer: " + blkRef.Layer.ToString() + ",\n");
-                    stringBuilder.Append(str);
-                    ed.WriteMessage(str);
-                    foreach (ObjectId attId in attCol)
+                    // перебираем аттрибуты
+                    foreach (ObjectId blkAttId in attCol)
+                    // btr.Dispose();
                     {
-                        AttributeReference attRef = (AttributeReference)tr.GetObject(attId, OpenMode.ForRead);
-                        // добавил условие по проверке название тега  ОБОЗНАЧ_КАБЕЛЯ
+                        AttributeReference attRef = (AttributeReference)tr.GetObject(blkAttId, OpenMode.ForRead);
+                        // btr.Dispose();
+                        //  выводим координаты блока,слой и handle
                         if (attRef.Tag == "ОБОЗНАЧ_КАБЕЛЯ")
                         {
-                            str = ("Attribute Tag: " + attRef.Tag + ",\n" +
-                                "Attribute String: " + attRef.TextString + ",\n");
+                            string str = ("\n--------------------------\n" +
+                                           "Block name: " + btr.Name + ",\n" +
+                                           "ID: " + blkRef.Id.ToString() + ",\n" +
+                                           "Attribute String: " + attRef.TextString + ",\n" +
+                                           "X: " + blkRef.Position.X.ToString() + ",\n" +
+                                           "Y: " + blkRef.Position.Y.ToString() + ",\n" +
+                                           "Z: " + blkRef.Position.Z.ToString() + ",\n" +
+                                           "Handle BlockRef : " + blkRef.Handle.ToString() + ",\n" + // вот нужная фигня - Handle
+                                           "Layer: " + blkRef.Layer.ToString() + ",\n");
+                            stringBuilder.Append(str);
                             ed.WriteMessage(str);
                         }
                     }
