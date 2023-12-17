6cd28cda4ca0c929e4509e8fec637c0f4fd8789e
fishman123456 <den9152@yandex.ru>
2023-12-17 21:00:49 +0700

    сделал выбор куда файл сохранять

--- BlockCoordSql/SaveCSV.cs
+++ BlockCoordSql/SaveCSV.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 
 using static System.Net.Mime.MediaTypeNames;
 
@@ -14,15 +15,21 @@ namespace ACADCommands
     {
         public async void saveCSV(string text)
         {
+            // открываем диалог для сохранения файла в поток
+            System.Windows.Forms.SaveFileDialog saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            saveFileDialog1.Filter = "Text files(*.csv)|*.csv|All files(*.*)|*.*";
+            // если не сохраняем то выходим
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string path = saveFileDialog1.FileName;
+            // сохраняем текст в файл
             try
             {
-
-            string path = "note1.csv";
-            //string text = "Hello World\nHello METANIT.COM";
-
             // полная перезапись файла 
             using (StreamWriter writer = new StreamWriter(path, false))
             {
+                    // асинхронная перезапись  файла
                 await writer.WriteAsync(text);
             }
                 // добавление в файл
@@ -31,11 +38,10 @@ namespace ACADCommands
                 //    await writer.WriteLineAsync("Addition");
                 //    await writer.WriteAsync("4,5");
                 //}
-
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                System.Windows.MessageBox.Show(ex.Message);
             }
         }
     }
